071b11d030b0303cf8e26ae79c27a5056efdc64b
Sherzod <99177182+Sherzod0411@users.noreply.github.com>
2022-08-08 22:16:27 +0300

    Update Homework3_5.java
    add last version to branch

--- src/homework3_1/Homework3_5.java
+++ src/homework3_1/Homework3_5.java
@@ -2,6 +2,12 @@ package homework3_1;
 
 public class Homework3_5 {
     public static void main(String[] args) {
-
+        for (int i = 0; i < 10; i++) {
+            if (i%2==1){
+                System.out.println(i*(-5)+",");
+            }else {
+                System.out.println(i*5+",");
+            }
+        }
     }
 }
